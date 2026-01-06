9a635945b8261fc8ed01d8e482d97abc1a4ec129
Thomson Lam <thomson.lam327@gmail.com>
2026-01-06 15:06:51 -0500

    added flash.nvim

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -38,7 +38,54 @@ return {
 			vim.keymap.set("n", "<C-f>", ":NvimTreeToggle<CR>", { desc = "Toggle file tree" })
 		end,
 	},
-
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Treesitter Search",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
+	},
 	-- oil.nvim for files
 	{
 		"stevearc/oil.nvim",
